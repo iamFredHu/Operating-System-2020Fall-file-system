b716ff49b22fd83a98df2bc91fe70327cfa2c437
iamFredHu <hucong@outlook.com>
2020-12-25 04:15:25 +0800

    删除cmake build文件夹

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/180110505/Operating-System-2020Fall-file-system
-# Build directory: /home/180110505/Operating-System-2020Fall-file-system/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
